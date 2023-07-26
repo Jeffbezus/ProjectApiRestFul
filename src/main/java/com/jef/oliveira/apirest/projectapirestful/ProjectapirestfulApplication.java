847dde168118c874309f934dfe3dd9ff4fc0bb3e
Jeffbezus <jefersonguedesdeoliveira@gmail.com>
2023-07-26 14:25:55 -0300

    Criação do projeto

---
+++ src/main/java/com/jef/oliveira/apirest/projectapirestful/ProjectapirestfulApplication.java
@@ -0,0 +1,13 @@
+package com.jef.oliveira.apirest.projectapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectapirestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectapirestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jef/oliveira/apirest/projectapirestful/ProjectapirestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jef.oliveira.apirest.projectapirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectapirestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
