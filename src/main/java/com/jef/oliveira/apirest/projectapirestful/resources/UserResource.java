fd446b136fa1ab5ef005cee3d687b1a7de1aaaeb
Jeffbezus <jefersonguedesdeoliveira@gmail.com>
2023-07-26 14:42:25 -0300

    user

---
+++ src/main/java/com/jef/oliveira/apirest/projectapirestful/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.jef.oliveira.apirest.projectapirestful.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jef.oliveira.apirest.projectapirestful.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        User maria = new User("1", "Maria", "maria@email.com");
+        User alex = new User("2", "Alex", "alex@email.com");
+        
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(maria, alex));
+
+        return ResponseEntity.ok().body(list);
+    }
+}
