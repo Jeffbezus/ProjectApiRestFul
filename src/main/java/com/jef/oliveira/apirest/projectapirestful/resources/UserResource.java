4fd5c6ae0c4c99eaf3a497f7f8a0b5b74fa24018
Jeffbezus <jefersonguedesdeoliveira@gmail.com>
2023-07-26 17:35:23 -0300

    implementando o padrao dto

--- src/main/java/com/jef/oliveira/apirest/projectapirestful/resources/UserResource.java
+++ src/main/java/com/jef/oliveira/apirest/projectapirestful/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.jef.oliveira.apirest.projectapirestful.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jef.oliveira.apirest.projectapirestful.domain.User;
+import com.jef.oliveira.apirest.projectapirestful.dto.UserDto;
 import com.jef.oliveira.apirest.projectapirestful.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDto>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList()); 
+        return ResponseEntity.ok().body(listDto);
     }
 }
